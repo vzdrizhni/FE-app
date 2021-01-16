4f340feaf0546248844a635adcda243b87b00eaa
vzdrizhni <vzdrizhni@gmail.com>
2021-01-16 21:07:02 +0500

    Add post editing

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -59,7 +59,9 @@ class Feed extends Component {
       })
       .then(resData => {
         this.setState({
-          posts: resData.posts,
+          posts: resData.posts.map(post => {
+            return {...post, imagePath: post.imageUrl}
+          }),
           totalPosts: resData.totalItems,
           postsLoading: false
         });
@@ -113,13 +115,14 @@ class Feed extends Component {
     let url = 'http://localhost:8080/feed/post';
     let method = 'POST'
     if (this.state.editPost) {
-      url = 'URL';
+      url = 'http://localhost:8080/feed/post/' + this.state.editPost._id;
+      method = 'PUT';
     }
 
     fetch(url, {
       method: method,
       body: formData,
-      mode: 'no-cors'
+      mode: 'cors'
     })
       .then(res => {
         if (res.status !== 200 && res.status !== 201) {
@@ -128,6 +131,7 @@ class Feed extends Component {
         return res.json();
       })
       .then(resData => {
+        console.log(resData);
         const post = {
           _id: resData.post._id,
           title: resData.post.title,
