6148c7769cd67211f241611f352fbfc92f926b97
vzdrizhni <vzdrizhni@gmail.com>
2021-01-18 18:02:51 +0500

    Add user signin

--- src/pages/Feed/SinglePost/SinglePost.js
+++ src/pages/Feed/SinglePost/SinglePost.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 import Image from '../../../components/Image/Image';
 import './SinglePost.css';
@@ -14,25 +14,26 @@ class SinglePost extends Component {
 
   componentDidMount() {
     const postId = this.props.match.params.postId;
-    fetch('http://localhost:8080/feed/post/' + postId)
-      .then(res => {
-        if (res.status !== 200) {
-          throw new Error('Failed to fetch status');
-        }
-        return res.json();
-      })
-      .then(resData => {
-        this.setState({
-          title: resData.post.title,
-          author: resData.post.creator.name,
-          image: 'http://localhost:8080/' + resData.post.imageUrl,
-          date: new Date(resData.post.createdAt).toLocaleDateString('en-US'),
-          content: resData.post.content
-        });
-      })
-      .catch(err => {
-        console.log(err);
+    fetch('http://localhost:8080/feed/post/' + postId, {
+      headers: {
+        Authorization: 'Bearer ' + this.props.token
+      }
+    }).then(res => {
+      if (res.status !== 200) {
+        throw new Error('Failed to fetch status');
+      }
+      return res.json();
+    }).then(resData => {
+      this.setState({
+        title: resData.post.title,
+        author: resData.post.creator.name,
+        image: 'http://localhost:8080/' + resData.post.imageUrl,
+        date: new Date(resData.post.createdAt).toLocaleDateString('en-US'),
+        content: resData.post.content
       });
+    }).catch(err => {
+      console.log(err);
+    });
   }
 
   render() {
@@ -40,10 +41,11 @@ class SinglePost extends Component {
       <section className="single-post">
         <h1>{this.state.title}</h1>
         <h2>
-          Created by {this.state.author} on {this.state.date}
+          Created by {this.state.author}
+          on {this.state.date}
         </h2>
         <div className="single-post__image">
-          <Image contain imageUrl={this.state.image} />
+          <Image contain imageUrl={this.state.image}/>
         </div>
         <p>{this.state.content}</p>
       </section>
